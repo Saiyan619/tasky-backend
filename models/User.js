ea46407bda865f811717e5f904f0d805c55c3b1e
olaniyi <arokoyueb11@gmail.com>
2024-12-28 16:15:53 +0100

    fixing add new user bug backend

--- models/User.js
+++ models/User.js
@@ -5,7 +5,6 @@ const UserSchema = new mongoose.Schema({
     clerkId: { type: String, required: true, unique: true }, // Clerk's user ID
     name: { type: String, required: true },                  // User's display name
     email: { type: String, required: true, unique: true },   // User's email
-    role: { type: String, enum: ['user', 'admin'], default: 'user' }, // Role for permissions
     createdAt: { type: Date, default: Date.now },            // User's registration date
 })
 
